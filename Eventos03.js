0f58887fc7283e0a5821b2b1243fb95ca571ec96
DawYorch <yorch100976@gmail.com>
2018-10-23 16:49:11 +0200

    Cambios

--- Eventos03.js
+++ Eventos03.js
@@ -1,14 +1,28 @@
     
     
-window.onload = function() {
+window.onload = function Escribe() {
 
                 document.getElementById('button01').onclick = mostrar;
+                document.getElementById('button02').addEventListener("click" , boton2 , false);
             }
 
 
-function mostrar(){
+            function mostrar(){
 
-alert("It's Alive");
+            alert("It's Alive"+this.id);
 
 
-    }        
+                }
+    
+            
+            function boton2(){
+
+
+                var evento = new Event('click');
+                document.getElementById("button01").dispatchEvent(evento);
+
+
+            }    
+
+    
+    

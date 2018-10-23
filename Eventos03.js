b383ab6f919d6018ed206edb12dc3c59b13ce637
DawYorch <43141639+DawYorch@users.noreply.github.com>
2018-10-23 16:18:55 +0200

    Eventos

---
+++ Eventos03.js
@@ -0,0 +1,14 @@
+    
+    
+window.onload = function() {
+
+                document.getElementById('button01').onclick = mostrar;
+            }
+
+
+function mostrar(){
+
+alert("It's Alive");
+
+
+    }        

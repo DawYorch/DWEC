486ea6045411f82031360cd7924debb4562bac7f
DawYorch <43141639+DawYorch@users.noreply.github.com>
2018-10-29 22:29:23 +0100

    Primer Paso

---
+++ Formulario_Extensible/Formulario_Extensible.js
@@ -0,0 +1,184 @@
+
+
+document.addEventListener("DOMContentLoaded",inicio,false);
+
+
+function inicio(){
+    document.getElementById("mas").addEventListener("click",nuevaFila);
+	document.getElementById("edi").addEventListener("click",Rellenar);
+	document.getElementById("eli").addEventListener("click",Eliminar01);
+}
+
+function Rellenar(){
+	
+    var array = this.parentNode.getElementsByTagName("input");	
+	this.disabled = true;
+	
+    for (let i = 0; i < array.length; i++) {
+        array[i].disabled = false;		
+    }
+		
+}
+
+function Eliminar01(){
+	
+	var borrar = true;
+    var array2 = this.parentNode.getElementsByTagName("input");		
+	
+    for (let i = 0; i < array2.length; i++) {
+        if(array2[i].value.length==0){
+			
+			borrar = false;
+			
+		}
+			
+		if (borrar){
+
+			Preguntar();		
+			
+		}		
+    }
+		
+}
+
+function Eliminar02(AAA){
+	
+
+    var arriba = AAA.parentNode;
+	var Mas = arriba.parentNode;
+   
+    Mas.removeChild(arriba);
+
+		
+}
+/* 
+    function eliminarElemento(id){
+    	imagen = document.getElementById(id);	
+    	if (!imagen){
+    		alert("El elemento selecionado no existe");
+    	} else {
+    		padre = imagen.parentNode;
+    		padre.removeChild(imagen);
+    	}
+    } */
+
+
+
+function Preguntar(){
+	
+	
+/* 	var dni = this.getElementById("inD");	
+	var nom = this.getElementById("inN");
+	var ape = this.getElementById("inA"); */
+	
+    var pregunta = confirm("¿Estas seguro de querer borrar");
+    
+	if (pregunta) {
+		
+        Eliminar02(this);
+    
+		}	
+}
+
+
+
+
+function nuevaFila(){
+	
+	var blok = document.getElementById("bloque");
+
+
+
+    //FILA
+    var renglonex = document.createElement("div");
+    renglonex.classList.add("form-inline");
+
+    //DNI
+    var dni= document.createElement("div");
+    var texDni = document.createTextNode("DNI");
+    var inDni = document.createElement("input");
+	inDni.setAttribute("id", "inD");
+	inDni.disabled = true;
+    inDni.classList.add("form-control");
+
+    //NOMBRE
+    var nombre = document.createElement("div");
+    var texNom = document.createTextNode("Nombre");
+    var inNom = document.createElement("input");
+	inNom.setAttribute("id", "inN");
+    inNom.disabled = true;
+    inNom.classList.add("form-control");
+    //APELLIDOS
+
+    var apellidos = document.createElement("div");
+    var texApel = document.createTextNode("Apellidos");
+    var inApel = document.createElement("input");
+	inApel.setAttribute("id", "inA");	
+    inApel.disabled = true;
+    inApel.classList.add("form-control");
+
+
+
+    //BORRAR
+
+    var eliminar = document.createElement("button");
+	eliminar.addEventListener("click",Eliminar01);
+	eliminar.setAttribute("id", "eli");
+	var texEli = document.createTextNode("X");
+    
+    eliminar.classList.add("btn-danger");
+	eliminar.classList.add("eliminar");
+
+    //EDITAR
+
+
+    var editar = document.createElement("button");
+	editar.addEventListener("click", Rellenar);
+	editar.setAttribute("id", "edi");
+	var texEdi = document.createTextNode("Abc");
+    
+    editar.classList.add("btn-success");
+	eliminar.classList.add("editar");
+	
+
+
+    //FILA
+	
+    blok.appendChild(renglonex);  //Crea un div dentro de "bloque2 donde insertar lo siguiente
+
+    //DNI
+		
+    dni.appendChild(texDni);   //dni crea un div .appenchild(textDni) introduce dentro el texto
+    dni.appendChild(inDni);
+	//
+    renglonex.appendChild(dni);
+	
+    
+    //NOMBRE
+	
+    nombre.appendChild(texNom);
+    nombre.appendChild(inNom);
+	
+    renglonex.appendChild(nombre);
+	
+
+
+    //APELLIDOS
+	
+    apellidos.appendChild(texApel);
+    apellidos.appendChild(inApel);
+    renglonex.appendChild(apellidos);
+
+
+    //BORRAR
+
+	eliminar.appendChild(texEli);
+	renglonex.appendChild(eliminar);
+
+    //EDITAR
+
+	editar.appendChild(texEdi);
+	renglonex.appendChild(editar);
+    
+
+}
